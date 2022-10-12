139546b825f5f76018977dd6688f3f6b79e1832e
Maurisz <p2002adam@gmail.com>
2022-10-12 12:21:25 +0200

    first commit

---
+++ src/index.ts
@@ -0,0 +1,11 @@
+
+
+let t = [1,2,3];
+t.filter(e => e > 2);
+
+document.addEventListener('DOMContentLoaded', () =>{
+    document.getElementById('szoveg')!.addEventListener('input',(e) : void =>{
+        let elem = e.currentTarget as HTMLInputElement;
+        document.body.style.background = elem.value;
+    });
+});
